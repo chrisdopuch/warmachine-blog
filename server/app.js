93f7aa04e3a1e65566f27d16dcd94b8ecd0cb21f
Chris Dopuch <cdopuch@appnexus.com>
2016-04-25 20:12:13 -0700

    adds babel

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
-var routes = require('./routes/index');
-var posts = require('./routes/posts');
+import routes from './routes/index';
+import posts from './routes/posts';
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +26,7 @@ app.use('/', routes);
 app.use('/posts', posts);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err);
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
+	app.use((err, req, res, next) =>  {
 		res.status(err.status || 500);
 		res.render('error', {
 			message: err.message,
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) =>  {
 	res.status(err.status || 500);
 	res.render('error', {
 		message: err.message,
